84f88863a904f6e180783a8928b9a6b1b569a1cb
JoseDev <joseslopez08k@gmail.com>
2025-09-18 13:45:53 +0200

    First commit

---
+++ src/test/java/com/github/Jose_Daniel_Lopez/github_activity_cli/GithubActivityCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Jose_Daniel_Lopez.github_activity_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActivityCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
