65d849bab03b028e1b10d3ada40d30a3a32ccb6f
JoseDev <joseslopez08k@gmail.com>
2025-09-22 14:07:51 +0200

    Add main application class for GitHub Activity CLI

---
+++ src/main/java/com/github/Jose_Daniel_Lopez/github_activity_cli/GithubActivityApplication.java
@@ -0,0 +1,12 @@
+package com.github.Jose_Daniel_Lopez.github_activity_cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActivityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActivityApplication.class, args);
+    }
+}
+
